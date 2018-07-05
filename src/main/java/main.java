d16793f1e389add2e5d938610e30c7e0793fd6cd
Michael Olivari <14128634+olimoli9160@users.noreply.github.com>
2018-07-05 14:17:07 +0200

    Update main.java

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,12 +2,34 @@ public class main {
 
     main(){}
     
-    void bogus2(){
+    /*void bogus2(){
         System.out.println("this function is not called");
-    }
+    }*/
 
     void helloWorld(){
         System.out.println("Hello, World!");
+        System.out.println("Hey, World!");
+        System.out.println("Hey, World!");
+        System.out.println("Hey, World!");
+        System.out.println("Hey, World!");
+        System.out.println("Hey, World!");
+        System.out.println("Hey, World!");
+        System.out.println("Hey, World!");
+        System.out.println("Hey, World!");
+        System.out.println("Hey, World!");
+        System.out.println("Hey, World!");
+        System.out.println("Hey, World!");
+        System.out.println("Hey, World!");
+        System.out.println("Hey, World!");
+        System.out.println("Hey, World!");
+        System.out.println("Hey, World!");
+        System.out.println("Hey, World!");
+        System.out.println("Hey, World!");
+        System.out.println("Hey, World!");
+        System.out.println("Hey, World!");
+        System.out.println("Hey, World!");
+        System.out.println("Hey, World!");
+        System.out.println("Hey, World!");
         System.out.println("Hey, World!");
     }
 
