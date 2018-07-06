38f119d05cedc89fe0e5437ff24fd83457f90012
Michael Olivari <14128634+olimoli9160@users.noreply.github.com>
2018-07-06 13:00:58 +0200

    Update main.java

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -62,35 +62,6 @@ public class main {
     //need to refactor
     void helloWorld(){
         System.out.println("Hello, World!");
-        System.out.println("Hey, World!");
-        System.out.println("Hey, World!");
-        System.out.println("Hey, World!");
-        System.out.println("Hey, World!");
-        System.out.println("Hey, World!");
-        System.out.println("Hey, World!");
-        System.out.println("Hey, World!");
-        System.out.println("Hey, World!");
-        System.out.println("Hey, World!");
-        System.out.println("Hey, World!");
-        System.out.println("Hey, World!");
-        System.out.println("Hey, World!");
-        System.out.println("Hey, World!");
-        System.out.println("Hey, World!");
-        System.out.println("Hey, World!");
-        System.out.println("Hey, World!");
-        System.out.println("Hey, World!");
-        System.out.println("Hey, World!");
-        System.out.println("Hey, World!");
-        System.out.println("Hey, World!");
-        System.out.println("Hey, World!");
-        System.out.println("Hey, World!");
-        System.out.println("Hey, World!");
-        System.out.println("Hey, World!");
-        System.out.println("Hey, World!");
-        System.out.println("Hey, World!");
-        System.out.println("Hey, World!");
-        System.out.println("Hey, World!");
-        System.out.println("Hey, World!");
     }
 
     void start(){
