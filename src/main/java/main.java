be589bf3e22a3b01ba5abca479342164321b8cde
Michael Olivari <14128634+olimoli9160@users.noreply.github.com>
2018-07-09 12:11:28 +0200

    Update main.java

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,64 +6,77 @@ public class main {
         System.out.println("this function is not called often!");
     }
     
+    void ints10(){
+       //int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        int e = 5;
+        int f = 6;
+        int g = 7;
+        int h = 8;
+        int i = 9;
+        int j = 10; 
+    }
+    
+    void ints20(){
+        int k = 11;
+        int l = 12;
+        int m = 13;
+        int n = 14;
+        int o = 15;
+        int p = 16;
+        int q = 17;
+        int r = 18;
+        int s = 19;
+        int t = 20;
+    }
+    
+    void ints30(){
+        int u = 21;
+        int v = 22;
+        int w = 23;
+        int x = 24;
+        int y = 25;
+        int z = 26;
+        int aa = 11;
+        int bb = 22;
+        int cc = 33;
+        int dd = 44;
+    }
+    
+    void ints40(){
+        int ee = 55;
+        int ff = 66;
+        int gg = 77;
+        int hh = 88;
+        int ii = 99;
+        int jj = 1010;
+        int kk = 1111;
+        int ll = 1212;
+        int mm = 1313;
+        int nn = 1414;
+    }
+    
+    void intsRemainder(){
+        int oo = 1515;
+        int pp = 1616;
+        int qq = 1717;
+        int rr = 1818;
+        int ss = 1919;
+        int tt = 2020;
+        int uu = 2121;
+        int vv = 2222;
+        int ww = 2323;
+        int xx = 2424;
+        int yy = 2525;
+        int zz = 2626;
+    }
     
     // we testing!
     void helloWorld(){
         System.out.println("Hello, World!");
         //System.out.println("Hello, World!");
-        //int a = 1;
-        
-    //int b = 2;
-    //int c = 3;
-    //int d = 4;
-    //int e = 5;
-    //int f = 6;
-    int g = 7;
-    int h = 8;
-    int i = 9;
-    int j = 10;
-    int k = 11;
-    int l = 12;
-    int m = 13;
-    int n = 14;
-    int o = 15;
-    int p = 16;
-    int q = 17;
-    int r = 18;
-    int s = 19;
-    int t = 20;
-    int u = 21;
-    int v = 22;
-    int w = 23;
-    int x = 24;
-    int y = 25;
-    int z = 26;
-    int aa = 11;
-    int bb = 22;
-    int cc = 33;
-    int dd = 44;
-    int ee = 55;
-    int ff = 66;
-    int gg = 77;
-    int hh = 88;
-    int ii = 99;
-    int jj = 1010;
-    int kk = 1111;
-    int ll = 1212;
-    int mm = 1313;
-    int nn = 1414;
-    int oo = 1515;
-    int pp = 1616;
-    int qq = 1717;
-    int rr = 1818;
-    int ss = 1919;
-    int tt = 2020;
-    int uu = 2121;
-    int vv = 2222;
-    int ww = 2323;
-    int xx = 2424;
-    int yy = 2525;
-    int zz = 2626;
     }
 
     void start(){
