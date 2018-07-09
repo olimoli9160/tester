614a253865ac03f6e0241054a1c3bac163120820
Michael Olivari <14128634+olimoli9160@users.noreply.github.com>
2018-07-09 11:33:06 +0200

    Update main.java

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -3,7 +3,7 @@ public class main {
     main(){}
     
     void bogus2(){
-        System.out.println("this function is not called");
+        System.out.println("this function is not called often!");
     }
     
     int a = 1;
@@ -59,7 +59,6 @@ public class main {
     int yy = 2525;
     int zz = 2626;
     
-    //need to refactor
     void helloWorld(){
         System.out.println("Hello, World!");
     }
