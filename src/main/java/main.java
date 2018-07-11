13031ceeeba5045225cb0cd17aa8e736a7eaa55d
Michael Olivari <14128634+olimoli9160@users.noreply.github.com>
2018-07-11 16:33:01 +0200

    Update main.java

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -62,13 +62,7 @@ public class main {
     //need to refactor
     void helloWorld(){
         System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
- 
+  
     }
 
     void start(){
