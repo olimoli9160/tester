8ec94a0b822a91f6384f5faf3a41abbd397d4a7c
Michael Olivari <14128634+olimoli9160@users.noreply.github.com>
2018-07-13 10:08:37 +0200

    guidelines lost

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -62,6 +62,24 @@ public class main {
     //need to refactor
     void helloWorld(){
         System.out.println("Hello, World!");
+                System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+
     }
 
     void start(){
