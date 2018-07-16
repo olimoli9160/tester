d21eef5067f8545f5544571ebf1dc2a5e4fc8199
Michael Olivari <14128634+olimoli9160@users.noreply.github.com>
2018-07-16 14:45:03 +0200

    Update main.java

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -61,6 +61,13 @@ public class main {
     
     void helloWorld(){
         System.out.println("Hello, World!");
+                System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+
     }
 
     void start(){
